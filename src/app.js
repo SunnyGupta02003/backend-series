b4bcca597ed70335d1a3a77f52b2d58af43e4f74
sunnyGupta128 <sunnygupta6497@gmail.com>
2024-07-17 17:22:17 +0530

    chore: Add express middleware for JSON parsing, URL encoding, static files, and cookie parsing

--- src/app.js
+++ src/app.js
@@ -9,5 +9,9 @@ app.use(cors({
     credentials: true
 }));
 
+app.use(express.json({limit: '16kb'}));
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'));
+app.use(cookieParser());
 
 export default app;
