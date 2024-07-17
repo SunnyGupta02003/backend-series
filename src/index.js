80b5a55ed06a06867d63a593c1e8dfe32e9697ab
sunnyGupta128 <sunnygupta6497@gmail.com>
2024-07-17 16:38:26 +0530

    chore: Update package.json with dotenv and mongoose dependencies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
-import express from 'express';
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "/.env"
+});
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    }
-);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+connectDB();
+
+
+
+
+
+
+/*
+
+(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        app.on("error", (error)=>{
+            console.error(`Error: ${error}`)
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`Server is running on port ${process.env.PORT}`)
+        })
+    }catch(error){
+        console.error(`Error: ${error}`)
     }
-);
+})();
+
+*/
+
