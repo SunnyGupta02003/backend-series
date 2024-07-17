a6f90eddc7fdf16b4711b959d7288e4fdfb99adf
sunnyGupta128 <sunnygupta6497@gmail.com>
2024-07-17 17:04:24 +0530

    Update package.json with cookie-parser and cors dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import express from "express";
+import app from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({
@@ -7,10 +7,18 @@ dotenv.config({
 });
 
 
-const app = express();
 
 
-connectDB();
+
+connectDB()
+.then(()=>{
+
+    app.listen(process.env.PORT || 8080, ()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+}).catch((error)=>{
+    console.log(`Error: ${error}`);
+});
 
 
 
