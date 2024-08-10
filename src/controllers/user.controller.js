312fd7cd1c44f00b5a054c73063288a7defa3769
sunnyGupta128 <sunnygupta6497@gmail.com>
2024-08-11 00:05:56 +0530

    feat: Add logout functionality and clear cookies on logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -163,7 +163,31 @@ const loginUser=asyncHandler(async(res, req)=>{
 })
 
 const logoutUser=asyncHandler(async(req, res)=>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            refreshToken: undefined
+        },
+        {
+            new: true
+        }
+    )
 
+    const options={
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res.status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(
+        new ApiResponse(200, {}, "Logout successfully")
+    )
 })
 
-export {registerUser}
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+}
